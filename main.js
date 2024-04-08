b27135dd462c187743e755b1fd21e6afda5b2421
luolinlang <48898373+luolinlang@users.noreply.github.com>
2024-04-08 10:47:00 +0800

    1

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// -*- coding: utf-8 -*-
+
 const { app, BrowserWindow, ipcMain, nativeTheme, globalShortcut,
-  dialog, shell, Notification , nativeImage 
+  dialog, shell, Notification , nativeImage ,Menu 
 } = require('electron/main')
 const path = require('node:path')
 
@@ -85,12 +87,174 @@ const createWindow = () => {
   ])
   mainWindow.setOverlayIcon(nativeImage.createFromPath('./images/a1.png'), 'Description for overlay')
 
+  let template = [
+    {
+      label: '文件',
+      submenu: [
+        {label: '打开',
+        accelerator: 'Ctrl+O',
+        click(){
+          console.log("open")
+        }
+        }
+      ]
+    },
+    {
+    label: '编辑',
+    submenu: [{
+      label: '撤销',
+      accelerator: 'CmdOrCtrl+Z',
+      role: 'undo'
+    }, {
+      label: '重做',
+      accelerator: 'Shift+CmdOrCtrl+Z',
+      role: 'redo'
+    }, {
+      type: 'separator'
+    }, {
+      label: '剪切',
+      accelerator: 'CmdOrCtrl+X',
+      role: 'cut'
+    }, {
+      label: '复制',
+      accelerator: 'CmdOrCtrl+C',
+      role: 'copy'
+    }, {
+      label: '粘贴',
+      accelerator: 'CmdOrCtrl+V',
+      role: 'paste'
+    }, {
+      label: '全选',
+      accelerator: 'CmdOrCtrl+A',
+      role: 'selectall'
+    }]
+  }, {
+    label: '查看',
+    submenu: [{
+      label: '重载',
+      accelerator: 'CmdOrCtrl+R',
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          // 重载之后, 刷新并关闭所有的次要窗体
+          if (focusedWindow.id === 1) {
+            BrowserWindow.getAllWindows().forEach(function (win) {
+              if (win.id > 1) {
+                win.close()
+              }
+            })
+          }
+          focusedWindow.reload()
+        }
+      }
+    }, {
+      label: '切换全屏',
+      accelerator: (function () {
+        if (process.platform === 'darwin') {
+          return 'Ctrl+Command+F'
+        } else {
+          return 'F11'
+        }
+      })(),
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          focusedWindow.setFullScreen(!focusedWindow.isFullScreen())
+        }
+      }
+    }, {
+      label: '切换开发者工具',
+      accelerator: (function () {
+        if (process.platform === 'darwin') {
+          return 'Alt+Command+I'
+        } else {
+          return 'Ctrl+Shift+I'
+        }
+      })(),
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          focusedWindow.toggleDevTools()
+        }
+      }
+    }, {
+      type: 'separator'
+    }, {
+      label: '应用程序菜单演示',
+      click: function (item, focusedWindow) {
+        if (focusedWindow) {
+          const options = {
+            type: 'info',
+            title: '应用程序菜单演示',
+            buttons: ['好的'],
+            message: '此演示用于 "菜单" 部分, 展示如何在应用程序菜单中创建可点击的菜单项.'
+          }
+          dialog.showMessageBox(focusedWindow, options, function () {})
+        }
+      }
+    }]
+  }, {
+    label: '窗口',
+    role: 'window',
+    submenu: [{
+      label: '最小化',
+      accelerator: 'CmdOrCtrl+M',
+      role: 'minimize'
+    }, {
+      label: '关闭',
+      accelerator: 'CmdOrCtrl+W',
+      role: 'close'
+    }, {
+      type: 'separator'
+    }, {
+      label: '重新打开窗口',
+      accelerator: 'CmdOrCtrl+Shift+T',
+      enabled: false,
+      key: 'reopenMenuItem',
+      click: function () {
+        app.emit('activate')
+      }
+    }]
+  }, {
+    label: '帮助',
+    role: 'help',
+    submenu: [{
+      label: '学习更多',
+      click: function () {
+        shell.openExternal('http://electron.atom.io')
+      }
+    }]
+  }]
 
+  // 获取当前应用程序菜单
+  const existingMenu = Menu.getApplicationMenu();
+
+  const translatedMenuTemplate = existingMenu.items.map(menuItem => {
+    if (menuItem.label === 'File') {
+      menuItem.label = '文件';
+    } else if (menuItem.label === 'Edit' || menuItem.label === '编辑') {
+      menuItem.label = '编辑1';
+    } else if (menuItem.label === 'View') {
+      menuItem.label = '视图';
+    }
+    return menuItem;
+  });
+
+  // 创建新的菜单模板
+  const newMenuTemplate = [
+    // 添加原有的菜单项
+    ...translatedMenuTemplate,
+    // 添加新的菜单项
+    ...template
+  ];
+
+  // 创建菜单对象
+  const newMenu = Menu.buildFromTemplate(newMenuTemplate);
+
+  // 将菜单应用到窗口
+  Menu.setApplicationMenu(newMenu);
 
   ipcMain.handle('dark-mode:toggle', () => {
     if (nativeTheme.shouldUseDarkColors) {
       nativeTheme.themeSource = 'light'
-    } else {4
+    } else {
       nativeTheme.themeSource = 'dark'
     }
     return nativeTheme.shouldUseDarkColors
